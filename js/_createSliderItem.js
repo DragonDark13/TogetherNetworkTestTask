5629a1125c0bdd1bbf13eecc422345c2a08f8ec8
AleksandrZachkevich <aleksdark13@gmail.com>
2023-11-03 20:41:51 +0200

    refactoring

--- js/_createSliderItem.js
+++ js/_createSliderItem.js
@@ -1,5 +1,3 @@
-import {createTextInput} from "./_createTextInput.js";
-
 export const createSlideElement = (number, stepSlideData,dataArrayLength) => {
     const newSlide = document.createElement('div');
     newSlide.className = "slide";
@@ -44,26 +42,6 @@ export const createSlideElement = (number, stepSlideData,dataArrayLength) => {
     return newSlide;
 }
 
-const createDropdownInput = (number, stepSlideData) => {
-    const dropdownContainer = document.createElement('div');
-    dropdownContainer.className = `dropdown_container dropdown_step${number}`;
-
-    dropdownContainer.innerHTML = `
-        <div class="dropdown_area" data-dropdown-value="">
-            <div class="dropdown_label">${stepSlideData.label}</div>
-            <div class="dropdown_input"></div>
-            <div class="dropdown_icon">
-                <img class="lightIcon" src="./images/arrowDropdown.svg" alt="arrowDropdown">
-                <img class="darkIcon" src="./images/arrowDropdownDark.svg" alt="arrowDropdownDark">
-            </div>
-        </div>
-        <ul class="dropdown_list">
-            ${generateDropdownOptions(stepSlideData.options)}
-        </ul>
-    `;
-
-    return dropdownContainer;
-}
 
 
 const createPrevButton = (number) => {
@@ -110,3 +88,88 @@ const generateDropdownOptions = (dropDownList) => {
 
     return options.join('');
 }
+
+export const createDropdownInput = (number, stepSlideData) => {
+    const dropdownContainer = document.createElement('div');
+    dropdownContainer.className = `dropdown_container dropdown_step${number}`;
+
+    dropdownContainer.innerHTML = `
+        <div class="dropdown_area" data-dropdown-value="">
+            <div class="dropdown_label">${stepSlideData.label}</div>
+            <div class="dropdown_input"></div>
+            <div class="dropdown_icon">
+                <img class="lightIcon" src="./images/arrowDropdown.svg" alt="arrowDropdown">
+                <img class="darkIcon" src="./images/arrowDropdownDark.svg" alt="arrowDropdownDark">
+            </div>
+        </div>
+        <ul class="dropdown_list">
+            ${generateDropdownOptions(stepSlideData.options)}
+        </ul>
+    `;
+
+    return dropdownContainer;
+}
+
+export const createTextInput = (stepSlideData) => {
+
+    const createContainer = (fieldName) => {
+        const container = document.createElement("label");
+        container.classList.add("input_container", "slider_input_container");
+        container.setAttribute("for", fieldName);
+        return container;
+    };
+
+    const createLabel = (text) => {
+        const label = document.createElement("div");
+        label.classList.add("custom_label");
+        label.textContent = text;
+        return label;
+    };
+
+    const createInput = (inputType, fieldName) => {
+        const input = document.createElement("input");
+        input.type = inputType;
+        input.id = fieldName;
+        input.name = fieldName;
+        input.maxLength = 20;
+        return input;
+    };
+
+    const createInvalidMessage = (text) => {
+        const message = document.createElement("div");
+        message.classList.add("invalid_message");
+        message.textContent = text;
+        return message;
+    };
+
+    const createInputBgBlock = () => {
+        const inputBgBlock = document.createElement("div");
+        inputBgBlock.classList.add("input_bg_block");
+        return inputBgBlock;
+    };
+
+    const newTextInputContainer = createContainer(stepSlideData.fieldName);
+    const label = createLabel(stepSlideData.label);
+    const input = createInput(stepSlideData.inputType, stepSlideData.fieldName);
+    const invalidMessage = createInvalidMessage("Невірний ввід");
+    const newInputBgBlock = createInputBgBlock();
+
+    const updateInputState = () => {
+        if (input.value.trim() !== '') {
+            newTextInputContainer.classList.add('filled');
+        } else {
+            newTextInputContainer.classList.remove('filled');
+        }
+    };
+
+    input.addEventListener('focus', updateInputState);
+    input.addEventListener('blur', updateInputState);
+    input.addEventListener('input', updateInputState);
+
+    newTextInputContainer.appendChild(input);
+    newTextInputContainer.appendChild(label);
+    newTextInputContainer.appendChild(newInputBgBlock);
+    newTextInputContainer.appendChild(invalidMessage);
+
+    return newTextInputContainer;
+};
